ecad4edf92512f6ab32189f438ce016fddbbead0
Critical Wombat <94728168+CriticalWombat@users.noreply.github.com>
2024-08-01 16:53:04 -0400

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,5 @@
+systemctl start docker && \
+curl -o /home/nexigen/greenbone/compose.yaml https://raw.githubusercontent.com/CriticalWombat/KickScan/dev/compose.yaml && \
+cd /home/nexigen/greenbone && \
+docker compose up -d
+echo 'Config script completed. Check for scanner interface at localhost:5555'
