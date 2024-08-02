f57caf5f7dd08ce7247e0d9f170458ba5a4f8826
Critical Wombat <94728168+CriticalWombat@users.noreply.github.com>
2024-08-01 22:58:42 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,6 +1,5 @@
-systemctl start docker && \
 mkdir /home/nexigen/greenbone
-curl -o /home/nexigen/greenbone/compose.yaml https://raw.githubusercontent.com/CriticalWombat/KickScan/dev/compose.yaml && \
-cd /home/nexigen/greenbone && \
+curl -o /home/nexigen/greenbone/compose.yaml https://raw.githubusercontent.com/CriticalWombat/KickScan/dev/compose.yaml
+cd /home/nexigen/greenbone
 docker compose -f /home/nexigen/greenbone/compose.yaml up -d
 echo 'Config script completed. Check for scanner interface at localhost:5555'
