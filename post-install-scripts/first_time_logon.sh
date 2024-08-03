28144f0c0039d457ec6cc3b34622ddc28880e965
Critical Wombat <criticalwombats@gmail.com>
2024-08-03 17:18:39 -0400

    change logic on new shell post docker deploy

--- post-install-scripts/first_time_logon.sh
+++ post-install-scripts/first_time_logon.sh
@@ -1,10 +1,13 @@
 ### Configure and Start Docker Compose for GreenBone Scanner. ###
-ip_address=$(hostname -I | awk '{print $1}')
+
 mkdir /home/nexigen/greenbone
 curl -o /home/nexigen/greenbone/compose.yaml https://raw.githubusercontent.com/CriticalWombat/KickScan/dev/yaml/compose.yaml
 docker compose -f /home/nexigen/greenbone/compose.yaml up -d
+
+# Ensure the script exits properly to allow user login to complete
+exec "$SHELL"
+ip_address=$(hostname -I | awk '{print $1}')
 echo ""
 echo "Docker configurator has completed. Check for the GreenBone scanner interface at http://$ip_address:5555" 
 echo ""
-
 ### Configure and Start Docker Compose for GreenBone Scanner. ###
