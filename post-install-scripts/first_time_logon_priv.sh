4ca23476c7f6a52cb76b4e6c504566a42a89316f
Critical Wombat <criticalwombats@gmail.com>
2024-08-03 19:14:11 -0400

    file permission changes

--- post-install-scripts/first_time_logon_priv.sh
+++ post-install-scripts/first_time_logon_priv.sh
@@ -1,25 +1,26 @@
 ### Make this editable only by root so that abuse of this script is not accessible to already setup devices. ###
 if [ -f "/home/nexigen/.first-logon-done" ]; then
-    chown root:root "/home/nexigen/.first-logon-done"
-    chmod 644 "/home/nexigen/.first-logon-done"
+    sudo chown root:root "/home/nexigen/.first-logon-done"
+    sudo chmod 644 "/home/nexigen/.first-logon-done"
 fi
 ### Make this editable only by root so that abuse of this script is not accessible to already setup devices. ###
 
 ##### Update /etc/issue to remove first time logon info #####
-cat << 'EOF' > /etc/issue
-__  ___      _       _____                 
-| |/ (_)    | |     / ____|                
-| ' / _  ___| | __ | (___   ___ __ _ _ __  
-|  < | |/ __| |/ /  \___ \ / __/ _` | '_ \ 
-| . \| | (__|   <   ____) | (_| (_| | | | |
-|_|\_\_|\___|_|\_\ |_____/ \___\__,_|_| |_|
-                                            
-                                            
-                        Welcome to KickScan
+sudo tee /etc/issue > /dev/null << 'EOF'
+ __  ___      _       _____                 
+ | |/ (_)    | |     / ____|                
+ | ' / _  ___| | __ | (___   ___ __ _ _ __  
+ |  < | |/ __| |/ /  \___ \ / __/ _` | '_ \ 
+ | . \| | (__|   <   ____) | (_| (_| | | | |
+ |_|\_\_|\___|_|\_\ |_____/ \___\__,_|_| |_|
+                                             
+                                             
+                         Welcome to KickScan
+
 
 
 EOF
 ##### Update /etc/issue to remove first time logon info #####
 
 # Delete this script since setuid is set and allows user execution with root permission
-rm -f -- "$0"
+sudo rm -f -- "$0"
