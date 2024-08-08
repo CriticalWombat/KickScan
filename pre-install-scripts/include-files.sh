aaf53899c93e3d162ec4d6bb2d39c4886d2d7351
Critical Wombat <Criticalwombats@gmail.com>
2024-08-08 15:17:56 -0400

    fix script execution for user/host creation

--- pre-install-scripts/include-files.sh
+++ pre-install-scripts/include-files.sh
@@ -2,15 +2,26 @@
 HOSTNAME_PREFIX="Scanner"
 ID=$(cat /proc/sys/kernel/random/uuid | cut -d "-" -f 3)
 HOSTNAME="${HOSTNAME_PREFIX}-${ID}"
-echo "network --bootproto=dhcp --hostname=${HOSTNAME}" > /tmp/host-include
+echo "network --bootproto=dhcp --hostname=${HOSTNAME}" > /tmp/host-include.ks
 
+# URL to fetch the username
+URL="https://usernameapiv1.vercel.app/api/random-usernames"
 # Generate a dynamic username and save it to a temporary file
-json=$(curl https://usernameapiv1.vercel.app/api/random-usernames)
-# Extract username using grep and sed
-username=$(echo $json | grep -oP '"usernames":\["\K[^"]+')
-# Remove trailing underscore using bash parameter expansion
-clean_username=${$username%_}
+response=$(curl -s -w "%{http_code}" -o /tmp/username.json $URL)
+# Check if the response code is 200
+if [ "${response: -3}" == "200" ]; then
+    # Extract the JSON part of the response
+    json=$(cat /tmp/username.json)
+    # Extract username
+    username=$(echo $json | grep -oP '"usernames":\["\K[^"]+')
+    # Remove trailing underscore using bash parameter expansion
+    clean_username=${username%_}
+else
+    # Set a static username
+    clean_username="user-$ID"
+fi
+
 # Make password
 password=$(tr -dc 'A-Za-z0-9!?%=' < /dev/urandom | head -c 10)
 # Add to the include statement
-echo "user --name=$clean_username --password=$password --plaintext" > /tmp/user-include
+echo "user --name=$clean_username --password=$password --plaintext" > /tmp/user-include.ks
